5be6fb7cb98fe82a496df9f2cd415be6afeea859
Asrivo Maha Kurnia Pitama <127371793+asrivo@users.noreply.github.com>
2023-05-16 11:58:20 +0700

    upload praktek buku alamat

---
+++ PBO_2201081004/src/Asrivo050523/DataBukuAlamat.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Asrivo050523;
+
+/**
+ *
+ * @author Asrivo MKP
+ */
+public class DataBukuAlamat {
+    
+}
